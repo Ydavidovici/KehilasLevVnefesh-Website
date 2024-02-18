9cf2d63126a1a9942d078bafb572f3c46de2ef79
Ydavidovici <150648286+Ydavidovici@users.noreply.github.com>
2024-02-18 14:58:13 -0500

    added styles.css sheet

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
+    <link rel="stylesheet" href="Styles.css">    
     <title>Kehilas Lev V'Nefesh</title>
     <style>
         /* Custom styles for the theme of rising sun and fire */
